5c7989f055fda819985378a487881019c48d9c6a
Omar <omar.assed@gmail.com>
2022-05-17 15:43:32 -0600

    commit tree 5cba8e995d4b7bc970e623b7f3494820b9cd2ed4

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block > 42) {
+        return block - 42;
+    } else if (block < 42) {
+        return 42-block;
+    }
+}
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    return Math.abs(start-destination) * 264;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+   if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+   } else if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) < 2000) {
+       return (distanceTravelledInFeet(start,destination) - 400) * 0.02;
+   } else if (distanceTravelledInFeet(start, destination) >= 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+       return 25;
+   } else if (distanceTravelledInFeet(start, destination) >= 2500) {
+       return "cannot travel that far";
+   }
+  } 
